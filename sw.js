918c3c2918dada9c2698f317dd49d0051fa31f6e
hiagomatias01 <ia.orcamentos.omil@gmail.com>
2026-09-09 10:48:40 -0300

    Atualiza documentos das maquinas (09/09/2026 10:48)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// Cache offline - versao 1df3558a9c62
-var CACHE = "qrdocs-1df3558a9c62";
+// Cache offline - versao 898344d7291f
+var CACHE = "qrdocs-898344d7291f";
 
 self.addEventListener("install", function () {
   self.skipWaiting();
